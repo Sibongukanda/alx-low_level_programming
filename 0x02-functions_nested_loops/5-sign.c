e340cbd1bf66707d43508b74be86073c3e7abf1a
Sibongukwanda <zwanesibongukwanda1998@gmail.com>
2023-02-22 00:58:19 +0200

    Update 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* _isalpha - Check main
+* @n: An input character
+* Description: prints the sign of a number.
+* Returns 1 and prints + if n is greater than zero
+* Returns 0 and prints 0 if n is zero
+* Returns -1 and prints - if n is less than zero.
+ */
+int print_sign(int n)
+{
+
+int num = 0;
+
+if (n > 0)
+{
+num = 1;
+_putchar('+');
+}else if (n == 0) {
+num = 0;
+_putchar('0');
+}
+else {
+num = -1;
+_putchar('-');
+}
+
+return (num);
+}
