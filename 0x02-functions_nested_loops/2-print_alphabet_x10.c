c98108467080538f18824256a653ed9e0ac6ffb3
Sibongukanda <zwanesibongukwanda1998@gmail.com>
2023-02-21 13:25:50 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,12 +6,16 @@
  * Return: Nothing.
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 char i;
 int x;
 for(x=0;x < 10 ;x++)
+{
 for (i = 'a'; i <= 'z'; i++)
+{
 _putchar(i);
+}
 _putchar('\n');
+}
 }
