7244702a6cd371fd0417ed4c5eba84396f1e23af
Sibongukwanda <zwanesibongukwanda1998@gmail.com>
2023-02-21 15:27:11 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,18 @@
 #include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
+*_isupper - function that verifies if a  character is uppercase or not
+*@c: tested character
+*Return: returns 1 if it is uppercase , 0 if not
+*/
+
+int _isupper(int c)
 {
-    char *sh = "_putchar";
 
-    while (*sh)
-    {
-        _putchar(*sh);
-        sh++;
-    }
-    _putchar('\n');
-
-    return (0);
+char[] put = "_putchar";
+for (int i = 0; i < 8; ++i)
+{
+   putchar(put[i]);
+}
+putchar('\n');
+return (0);
 }
