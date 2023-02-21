2520cd9677a77501a5f2e7bda3fc05c89b830aa8
Sibongukwanda <zwanesibongukwanda1998@gmail.com>
2023-02-22 01:07:50 +0200

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,9 +4,9 @@
 * print_sign - Check main
 * @n: An input character
 * Description: function returns 1 0 -1 based on the n
-* Returns 1 and prints + if n is greater than zero
-* Returns 0 and prints 0 if n is zero
-* Returns -1 and prints - if n is less than zero.
+* Returns: 1 and prints + if n is greater than zero
+* Returns: 0 and prints 0 if n is zero
+* Returns: -1 and prints - if n is less than zero.
  */
 
 int print_sign(int n)
