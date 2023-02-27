7b9715eb3cd658ef155a50d89305b0afe158222b
Sibongukwanda <zwanesibongukwanda1998@gmail.com>
2023-02-27 17:17:25 +0200

    print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,20 +1,20 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_rev - a function that prints a string, in reverse,
- * followed by a new line.
- * @s: an input string
- * Return: Nothing
- */
+* print_rev - a function that prints a string, in reverse,
+* followed by a new line.
+* @s: an input string
+* Return: Nothing
+*/
 void print_rev(char *s)
 {
-	int len = 0;
+int len = 0;
 
-	while (s[len] != '\0')
-		len++;
+while (s[len] != '\0')
+len++;
 
-	while (len)
-		_putchar(s[--len]);
+while (len)
+_putchar(s[--len]);
 
-	_putchar('\n');
+_putchar('\n');
 }
