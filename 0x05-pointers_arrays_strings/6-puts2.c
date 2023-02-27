089af9effeb932c5a37992134a99fd22e4ec2215
Sibongukwanda <zwanesibongukwanda1998@gmail.com>
2023-02-27 17:22:12 +0200

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,22 +1,22 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * puts2 - a function that prints every other character of a string,
- * starting with the first character, followed by a new line.
- * @str: An input string
- * Return: Nothing
- */
+* puts2 - a function that prints every other character of a string,
+* starting with the first character, followed by a new line.
+* @str: An input string
+* Return: Nothing
+*/
 void puts2(char *str)
 {
-	int len = 0, i = 0;
+int len = 0, i = 0;
 
-	while (str[len] != '\0')
-		len++;
+while (str[len] != '\0')
+	len++;
 
-	len -= 1;
+len -= 1;
 
-	for (; i <= len; i += 2)
-		_putchar(str[i]);
+for (; i <= len; i += 2)
+	_putchar(str[i]);
 
-	_putchar('\n');
+_putchar('\n');
 }
