44761090bf65b31e9271e28067c046405b2b9b94
Sibongukwanda <101362253+Sibongukanda@users.noreply.github.com>
2023-03-02 21:43:28 +0200

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,34 +1,34 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _strncat - a function that concatenates two strings.
- * @dest: an input string
- * @src: an input string
- * @n: an input integer
- * Return: A pointer to the resulting string
- */
+* _strncat - a function that concatenates two strings.
+* @dest: an input string
+* @src: an input string
+* @n: an input integer
+* Return: A pointer to the resulting string
+*/
 char *_strncat(char *dest, char *src, int n)
 {
-	int srclen = 0, i = 0;
-	char *temp = dest, *start = src;
+int srclen = 0, i = 0;
+char *temp = dest, *start = src;
 
-	while (*src)
-	{
-		srclen++;
-		src++;
-	}
+while (*src)
+{
+srclen++;
+src++;
+}
 
-	while (*dest)
-		dest++;
+while (*dest)
+dest++;
 
-	if (n > srclen)
-		n = srclen;
+if (n > srclen)
+n = srclen;
 
-	src = start;
+src = start;
 
-	for (; i < n; i++)
-		*dest++ = *src++;
+for (; i < n; i++)
+*dest++ = *src++;
 
-	*dest = '\0';
-	return (temp);
+*dest = '\0';
+return (temp);
 }
